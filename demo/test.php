fd7df8916e97989075676e21d0ef5f64b284b65e
熊龙华 <xionglonghua@xionglonghuadeMacBook-Pro.local>
2018-09-05 15:28:04 +0800

    composer封装

---
+++ demo/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xionglonghua
+ * Date: 2018/9/4
+ * Time: 下午4:35
+ */
+
+
+require(__DIR__ . '/../vendor/autoload.php');
+//require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+//require(__DIR__ . '/../Dingtalk.php');
+
+
+
+
+//$dingding = new \lspbupt\dingtalk\Dingtalk();
+//var_dump($dingding->setGet()->httpExec("/get_jsapi_ticket", []));
---
+++ jtExpress.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * User: xionglonghua
+ * Date: 2018/9/4
+ * Time: 下午3:11
+ */
+
+namespace lspbupt\jtExpress;
+
+use \Yii;
+use yii\caching\Cache;
+use yii\di\Instance;
+
+class JtExpress extends \lspbupt\curl\CurlHttp
+{
+    public $orderNumber = '';
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function getOrderInfo()
+    {
+        return 'test';
+    }
+
+}
