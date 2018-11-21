2f2bfceb6262e694fb4aa44c6ccbc010d6e0b75f
xionglonghua <longhuadaxia@gmail.com>
2018-11-21 10:45:12 +0700

    增加取消订单接口

--- JtExpress.php
+++ JtExpress.php
@@ -42,6 +42,11 @@ class JtExpress extends \lspbupt\curl\CurlHttp
         };
     }
 
+    /**
+     * 查询快递信息
+     * @param int $billcode
+     * @return mixed
+     */
     public function getOrderInfo($billcode = 0)
     {
         if (isset(Yii::$app->params['express']['jnt']['trackingHost'])) {
@@ -61,6 +66,11 @@ class JtExpress extends \lspbupt\curl\CurlHttp
         return $this->setPost()->httpExec('/jandt_track/trackToJson.action', $data_request);
     }
 
+    /**
+     * 快递下单
+     * @param $params
+     * @return mixed
+     */
     public function onlineOrder($params)
     {
         if (isset(Yii::$app->params['express']['jnt']['orderHost'])) {
@@ -79,6 +89,34 @@ class JtExpress extends \lspbupt\curl\CurlHttp
             'data_sign'=> base64_encode(md5($data_json.$this->key)),
         ];
         return $this->setPost()->httpExec('/JandT_ecommerce/api/onlineOrder.action', $data_request);
+    }
+
+    /**
+     * 取消订单
+     * @param int $billCode
+     * @param string $remark
+     * @return mixed
+     */
+    public function cancelOrder($billCode = 0, $remark = '')
+    {
+        if (isset(Yii::$app->params['express']['jnt']['cancelHost'])) {
+            $this->host = Yii::$app->params['express']['jnt']['cancelHost'];
+        } else {
+            throw new InvalidParamException("Please configure param: cancelHost");
+        }
+        $remark = empty($remark) ? 'Canceled by '.$this->username : $remark;
+        $data = [
+            'username' => $this->username,
+            'api_key'  => $this->apiKey,
+            'orderid'  => $billCode,
+            'remark'   => $remark,
+        ];
+        $data_json = json_encode(['detail'=> [$data]]);
+        $data_request = [
+            'data_param'=>$data_json,
+            'data_sign'=> base64_encode(md5($data_json.$this->key)),
+        ];
+        return $this->setPost()->httpExec('/JandT_ecommerce/api/cancelOrder.action', $data_request);
     }
 
 }
